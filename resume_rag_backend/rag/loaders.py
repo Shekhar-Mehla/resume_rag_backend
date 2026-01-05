790b71eee5fc28c3924d9b3d7c05e686d22726ba
Shekhar Singh <smehla147@gmail.com>
2026-01-05 13:33:29 +1100

    setup the pdf loader

--- resume_rag_backend/rag/loaders.py
+++ resume_rag_backend/rag/loaders.py
@@ -1,2 +1,14 @@
-def loader():
-    
+from langchain_community.document_loaders import PyPDFLoader
+from pathlib import Path
+
+# Path to the PDF (two folders up from this script)
+file_path = Path(__file__).parent.parent / "masterResume.pdf"
+
+def file_loader():
+    """Load the data from the PDF file"""
+    if file_path.exists():
+        loader = PyPDFLoader(str(file_path))  # convert Path to string
+        data = loader.load()
+        return data
+    else:
+        raise Exception(f"File not found: {file_path}")
--- resume_rag_backend/rag/views.py
+++ resume_rag_backend/rag/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializer import ResumeSerializer
+from .loaders import file_loader
 
 
 class ResumeView(APIView):
@@ -12,5 +13,8 @@ class ResumeView(APIView):
         serializer = ResumeSerializer(data=request.data)
         if serializer.is_valid():
             # Process the resume data
-            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+            p =file_loader()
+            print(p)
+            
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
